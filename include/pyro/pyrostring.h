286fbe68de6fc383d67b91769343789b1cce8b59
Greger Stolt Nilsen <gregersn@gmail.com>
2020-05-03 16:24:59 +0200

    Fix unknown typename on mac

--- include/pyro/pyrostring.h
+++ include/pyro/pyrostring.h
@@ -3,6 +3,6 @@
 
 #include <string>
 namespace Pyro {
-    std::string nf(uint number, uint digits);
+    std::string nf(uint64_t number, uint64_t digits);
 }
 #endif
--- src/pyrostring.cpp
+++ src/pyrostring.cpp
@@ -4,7 +4,7 @@
 
 namespace Pyro
 {
-    std::string nf(uint number, uint digits) {
+    std::string nf(uint64_t number, uint64_t digits) {
         std::string number_string = std::to_string(number);
         return std::string(std::max((int)(digits - number_string.size()), 0), '0') + number_string;
     }
