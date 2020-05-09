896938364b2dbfec6142de448205d4fdcb8bbfc0
Greger Stolt Nilsen <gregersn@gmail.com>
2020-05-09 14:36:01 +0200

    Get rid of a warning.

--- screenshot-tests/test-settings.h
+++ screenshot-tests/test-settings.h
@@ -6,7 +6,7 @@
 
 static inline std::string const actual_folder = "../screenshot-tests/actual/";
 static inline std::string const current_folder = "./current/";
-static inline Pyro::GraphicsMode testmode = Pyro::GraphicsMode::CAIRO;
+constexpr static inline Pyro::GraphicsMode testmode = Pyro::GraphicsMode::CAIRO;
 
 class ImageMatch : public Catch::MatcherBase<std::string> {
     std::string actual_file;
