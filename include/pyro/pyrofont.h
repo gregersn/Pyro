68c1ae6062f66819f039e09060f455565d591e6d
Greger Stolt Nilsen <gregersn@gmail.com>
2020-07-26 10:48:48 +0200

    Refactor font to hide freetype.

--- include/pyro/pyrofont.h
+++ include/pyro/pyrofont.h
@@ -2,19 +2,17 @@
 #define PYROFONT_H
 #include <string>
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
 namespace Pyro {
+    class FontImpl;
+
     class Font {
         private:
             std::string filename = "";
-            FT_Face face = nullptr;
 
         public:
             Font(std::string filename);
             static Font * load(std::string filename);
-            FT_Face get_ft_face() { return this->face; };
+            FontImpl *impl;
     };
 
     Font * create_font(std::string filename, int size);
