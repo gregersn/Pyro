48b35e95fde6a0dcbc783940e2cf5187fd9301a3
Greger Stolt Nilsen <greger@fusetools.com>
2021-03-26 23:01:40 +0100

    Fix background command to set the chosen color.

--- tests/test-graphics.cpp
+++ tests/test-graphics.cpp
@@ -2,8 +2,10 @@
 
 #include <pyro/pyrographics.h>
 
-TEST_CASE("Graphics objects can be initialized", "[graphics]") {
-    SECTION("Create a graphics object") {
+TEST_CASE("Graphics objects can be initialized", "[graphics]")
+{
+    SECTION("Create a graphics object")
+    {
         Pyro::Graphics *graphics = Pyro::Graphics::create(400, 300);
 
         REQUIRE(graphics->width() == 400);
@@ -11,24 +13,28 @@ TEST_CASE("Graphics objects can be initialized", "[graphics]") {
     }
 }
 
-TEST_CASE("Graphics can have backgrounds", "[graphics]") {
+TEST_CASE("Graphics can have backgrounds", "[graphics]")
+{
     Pyro::Graphics *pg = Pyro::Graphics::create(4, 4);
 
-    SECTION("Set a red background") {
+    SECTION("Set a red background")
+    {
         pg->background(1.0f, 0.0f, 0.0f);
         uint32_t p = pg->get(0, 0);
 
         REQUIRE(p == 0xffff0000);
     }
 
-    SECTION("Set a green background") {
+    SECTION("Set a green background")
+    {
         pg->background(0.0f, 1.0f, 0.0f);
         uint32_t p = pg->get(0, 0);
 
         REQUIRE(p == 0xff00ff00);
     }
 
-    SECTION("Set a blue background") {
+    SECTION("Set a blue background")
+    {
         pg->background(0.0f, 0.0f, 1.0f);
         uint32_t p = pg->get(0, 0);
 
@@ -36,10 +42,13 @@ TEST_CASE("Graphics can have backgrounds", "[graphics]") {
     }
 }
 
-TEST_CASE("Fill can be set", "[graphics]") {
+TEST_CASE("Fill can be set", "[graphics]")
+{
     Pyro::Graphics *pg = Pyro::Graphics::create(8, 8);
 
-    SECTION("Set a red fill") {
+    SECTION("Set a red fill")
+    {
+        pg->background(0, 0);
         pg->fill(1.0f, 0.0f, 0.0f);
         pg->nostroke();
         pg->rect(2, 2, 4, 4);
@@ -52,10 +61,12 @@ TEST_CASE("Fill can be set", "[graphics]") {
     }
 }
 
-TEST_CASE("Stroke can be set", "[graphics]") {
+TEST_CASE("Stroke can be set", "[graphics]")
+{
     Pyro::Graphics *pg = Pyro::Graphics::create(10, 1);
 
-    SECTION("Set no stroke") {
+    SECTION("Set no stroke")
+    {
         pg->nostroke();
         pg->nosmooth();
         pg->nofill();
@@ -68,7 +79,9 @@ TEST_CASE("Stroke can be set", "[graphics]") {
         REQUIRE(p == q);
     }
 
-    SECTION("Set red stroke") {
+    SECTION("Set red stroke")
+    {
+        pg->background(0, 0);
         pg->nofill();
         pg->nosmooth();
         pg->stroke(1.0f, 0.0f, 0.0f, 1.0f);
@@ -83,10 +96,13 @@ TEST_CASE("Stroke can be set", "[graphics]") {
     }
 }
 
-TEST_CASE("Shapes can be drawn", "[graphics]") {
+TEST_CASE("Shapes can be drawn", "[graphics]")
+{
     Pyro::Graphics *pg = Pyro::Graphics::create(8, 8);
 
-    SECTION("Draw a filled square") {
+    SECTION("Draw a filled square")
+    {
+        pg->background(0, 0);
         pg->nosmooth();
         pg->nostroke();
         pg->fill(0.0f, 1.0f, 0.0f, 1.0f);
@@ -104,10 +120,10 @@ TEST_CASE("Shapes can be drawn", "[graphics]") {
 
         uint32_t q = pg->get(4, 4);
         REQUIRE(q == 0xff00ff00);
-
     }
 
-    SECTION("Draw with curvevertex") {
+    SECTION("Draw with curvevertex")
+    {
         pg->nostroke();
         pg->fill(0.0f, 1.0f, 0.0f, 1.0f);
 
@@ -120,8 +136,10 @@ TEST_CASE("Shapes can be drawn", "[graphics]") {
         pg->endshape();
     }
 
-    SECTION("Draw rect") {
-        SECTION("RectMode default") {
+    SECTION("Draw rect")
+    {
+        SECTION("RectMode default")
+        {
             /**
              *     11110000
              *     11110000
@@ -132,6 +150,7 @@ TEST_CASE("Shapes can be drawn", "[graphics]") {
              *     00000000
              *     00000000
              * */
+            pg->background(0, 0);
             pg->nosmooth();
             pg->nostroke();
             pg->fill(1.0f);
@@ -150,7 +169,8 @@ TEST_CASE("Shapes can be drawn", "[graphics]") {
             remove("s.png");
         }
 
-        SECTION("RectMode center") {
+        SECTION("RectMode center")
+        {
             /**
              *     11000000
              *     11000000
@@ -161,6 +181,7 @@ TEST_CASE("Shapes can be drawn", "[graphics]") {
              *     00000000
              *     00000000
              * */
+            pg->background(0, 0);
             pg->nosmooth();
             pg->rectmode(Pyro::CENTER);
             pg->nostroke();
@@ -182,10 +203,12 @@ TEST_CASE("Shapes can be drawn", "[graphics]") {
     }
 }
 
-TEST_CASE("Select different color modes") {
+TEST_CASE("Select different color modes")
+{
     Pyro::Graphics *pg = Pyro::Graphics::create(3, 3);
 
-    SECTION("Use HSB") {
+    SECTION("Use HSB")
+    {
         pg->nosmooth();
         pg->nostroke();
         pg->colormode(Pyro::HSB);
@@ -197,10 +220,12 @@ TEST_CASE("Select different color modes") {
     }
 }
 
-TEST_CASE("Save and load images") {
+TEST_CASE("Save and load images")
+{
     Pyro::Graphics *pg = Pyro::Graphics::create(4, 4);
     const char *filename = "__4x4_ARGB__.png";
-    SECTION("Create, save and load") {
+    SECTION("Create, save and load")
+    {
         pg->background(1.0f, 0.0f, 1.0f, 1.0f);
         pg->save(filename);
 
