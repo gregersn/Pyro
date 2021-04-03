b41a1b62c4c3b58c904696d44d22263f92734f2f
Greger Stolt Nilsen <gregersn@gmail.com>
2021-04-03 17:08:47 +0200

    Some more shape drawing.

--- src/pyrographics_gsn.cpp
+++ src/pyrographics_gsn.cpp
@@ -11,56 +11,6 @@ namespace Pyro
     {
     }
 
-    void GraphicsGSN::shape(Shape s, float x, float y)
-    {
-        /*cairo_save(this->cr);
-        cairo_new_path(this->cr);
-
-        cairo_set_line_width(this->cr, this->stroke_weight);
-
-        cairo_translate(this->cr, x, y);*/
-
-        for (size_t contour_index = 0; contour_index < s.getpoints().size(); contour_index++)
-        {
-            std::vector<Pyro::Vector> contour_points = s.getpoints()[contour_index];
-
-            for (size_t i = 0; i < contour_points.size(); i++)
-            {
-                if (i == 0)
-                {
-                    //cairo_move_to(this->cr, contour_points[i].x, contour_points[i].y);
-                }
-                else
-                {
-                    //cairo_line_to(this->cr, contour_points[i].x, contour_points[i].y);
-                }
-            }
-            if (s.close == CLOSE)
-            {
-                //cairo_close_path(this->cr);
-            }
-        }
-        if (this->fill_enable)
-        {
-            /*cairo_set_source_rgba(this->cr, this->fill_color.r,
-                                  this->fill_color.g,
-                                  this->fill_color.b,
-                                  this->fill_color.a);
-            cairo_fill_preserve(this->cr);*/
-        }
-
-        if (this->stroke_enable)
-        {
-            /*cairo_set_source_rgba(this->cr, this->stroke_color.r,
-                                  this->stroke_color.g,
-                                  this->stroke_color.b,
-                                  this->stroke_color.a);
-            cairo_stroke(this->cr);*/
-        }
-
-        //cairo_restore(this->cr);
-    }
-
     void GraphicsGSN::line(float x0, float y0, float x1, float y1)
     {
         Pyro::Vector a = this->transformer.apply(Pyro::Vector(x0, y0));
@@ -106,6 +56,57 @@ namespace Pyro
             {
                 this->set(int(x), int(y), this->stroke_color.to_uint());
                 x += a;
+            }
+        }
+    }
+    void GraphicsGSN::shape(Shape s, float x, float y)
+    {
+        for (uint c = 0; c < s.getpoints().size(); c++)
+        {
+            auto points = s.getpoints()[c];
+            if (this->fill_enable)
+            {
+                float minx = points[0].x;
+                float maxx = points[0].x;
+                float miny = points[0].y;
+                float maxy = points[0].y;
+                for (size_t i = 0; i < points.size(); i++)
+                {
+                    minx = std::min(minx, points[i].x);
+                    miny = std::min(miny, points[i].y);
+                    maxx = std::max(maxx, points[i].x);
+                    maxy = std::max(maxy, points[i].y);
+                }
+                for (uint y = miny; y <= maxy; y++)
+                {
+                    uint32_t col = this->fill_color.to_uint();
+                    for (uint x = minx; x <= maxx; x++)
+                    {
+                        Vector p = Vector(x, y);
+                        if (s.is_point_in_path(p))
+                        {
+                            Vector t = this->transformer.apply(p);
+                            this->set(int(t.x), int(t.y), col);
+                        }
+                    }
+                }
+            }
+            if (this->stroke_enable)
+            {
+                for (size_t i = 0; i < points.size() - 1; i++)
+                {
+                    auto p0 = this->transformer.apply(points[i]);
+                    auto p1 = this->transformer.apply(points[i + 1]);
+
+                    this->draw_line(p0.x, p0.y, p1.x, p1.y);
+                }
+                if (s.close == CLOSE)
+                {
+                    auto p0 = this->transformer.apply(points[points.size() - 1]);
+                    auto p1 = this->transformer.apply(points[0]);
+
+                    this->draw_line(p0.x, p0.y, p1.x, p1.y);
+                }
             }
         }
     }
