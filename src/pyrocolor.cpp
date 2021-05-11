797f2e2fdc667f1c62200c70ab25b0ede320d881
Greger Stolt Nilsen <gregersn@gmail.com>
2021-05-11 11:35:54 +0200

    Add more color handling functions.

--- src/pyrocolor.cpp
+++ src/pyrocolor.cpp
@@ -15,10 +15,10 @@ namespace Pyro
         case Pyro::RGB:
         case Pyro::RGBA:
         default:
-            this->r = a;
-            this->g = b;
-            this->b = c;
-            this->a = d;
+            this->_red = a;
+            this->_green = b;
+            this->_blue = c;
+            this->_alpha = d;
             break;
         }
     }
@@ -64,10 +64,160 @@ namespace Pyro
 
         float m = brightness - C;
 
-        this->r = R1 + m;
-        this->g = G1 + m;
-        this->b = B1 + m;
-        this->a = a;
+        this->_red = R1 + m;
+        this->_green = G1 + m;
+        this->_blue = B1 + m;
+        this->_alpha = a;
+    }
+
+    void Color::update_hsl()
+    {
+        if (this->dirty_hsl)
+        {
+            float Xmax = max(max(this->_red, this->_green), this->_blue);
+            float Xmin = min(min(this->_red, this->_green), this->_blue);
+
+            if (Xmax == Xmin)
+            {
+                this->_hue = 0.0f;
+                this->_saturation = 0.0f;
+                this->_lightness = this->_red;
+            }
+            else
+            {
+                this->_lightness = (Xmax + Xmin) / 2.0f;
+                if (this->_lightness < 0.5f)
+                {
+                    this->_saturation = (Xmax - Xmin) / (Xmax + Xmin);
+                }
+                else
+                {
+                    this->_saturation = (Xmax - Xmin) / (2.0f - Xmax - Xmin);
+                }
+
+                if (this->_red == Xmax)
+                    this->_hue = (this->_green - this->_blue) / (Xmax - Xmin);
+                else if (this->_green == Xmax)
+                    this->_hue = 2.0 + (this->_blue - this->_red) / (Xmax - Xmin);
+                else
+                    this->_hue = 4.0 + (this->_red - this->_green) / (Xmax - Xmin);
+
+                this->_hue /= 6.0f;
+
+                this->dirty_hsl = false;
+            }
+        }
+    }
+
+    void Color::update_rgb()
+    {
+        if (this->dirty_rgb)
+        {
+            if (this->_saturation == 0.0)
+            {
+                this->_red = this->_lightness;
+                this->_green = this->_lightness;
+                this->_blue = this->_lightness;
+            }
+            else
+            {
+                float temp2;
+                if (this->_lightness < 0.5)
+                    temp2 = this->_lightness * (1.0 + this->_saturation);
+                else
+                    temp2 = (this->_lightness + this->_saturation) - (this->_lightness * this->_saturation);
+
+                float temp1;
+                temp1 = 2.0 * this->_lightness - temp2;
+                float tempr = this->_hue + 1.0 / 3.0;
+                if (tempr > 1)
+                    tempr--;
+                float tempg = this->_hue;
+                float tempb = this->_hue - 1.0 / 3.0;
+                if (tempb < 0)
+                    tempb++;
+
+                if (tempr < 1.0 / 6.0)
+                    this->_red = temp1 + (temp2 - temp1) * 6.0 * tempr;
+                else if (tempr < 0.5)
+                    this->_red = temp2;
+                else if (tempr < 2.0 / 3.0)
+                    this->_red = temp1 + (temp2 - temp1) * ((2.0 / 3.0) - tempr) * 6.0;
+                else
+                    this->_red = temp1;
+
+                if (tempg < 1.0 / 6.0)
+                    this->_green = temp1 + (temp2 - temp1) * 6.0 * tempg;
+                else if (tempg < 0.5)
+                    this->_green = temp2;
+                else if (tempg < 2.0 / 3.0)
+                    this->_green = temp1 + (temp2 - temp1) * ((2.0 / 3.0) - tempg) * 6.0;
+                else
+                    this->_green = temp1;
+
+                if (tempb < 1.0 / 6.0)
+                    this->_blue = temp1 + (temp2 - temp1) * 6.0 * tempb;
+                else if (tempb < 0.5)
+                    this->_blue = temp2;
+                else if (tempb < 2.0 / 3.0)
+                    this->_blue = temp1 + (temp2 - temp1) * ((2.0 / 3.0) - tempb) * 6.0;
+                else
+                    this->_blue = temp1;
+            }
+
+            this->dirty_rgb = false;
+        }
+    }
+
+    float Color::lightness()
+    {
+        this->update_hsl();
+        return this->_lightness;
+    }
+
+    float Color::hue()
+    {
+        this->update_hsl();
+        return this->_hue;
+    }
+
+    float Color::saturation()
+    {
+        this->update_hsl();
+        return this->_saturation;
+    }
+
+    float Color::r()
+    {
+        this->update_rgb();
+        return this->_red;
+    }
+
+    float Color::g()
+    {
+        this->update_rgb();
+        return this->_green;
+    }
+
+    float Color::b()
+    {
+        this->update_rgb();
+        return this->_blue;
+    }
+
+    float Color::a()
+    {
+        return this->_alpha;
+    }
+
+    void Color::lightness(float b)
+    {
+        if (this->dirty_hsl && !this->dirty_rgb)
+        {
+            this->update_hsl();
+        }
+        this->_lightness = b;
+        this->dirty_rgb = true;
     }
 
     Palette::Palette() : colors(std::vector<Color>())
@@ -114,5 +264,4 @@ namespace Pyro
     {
         return a << 24 | r << 16 | g << 8 | b;
     }
-
 }
