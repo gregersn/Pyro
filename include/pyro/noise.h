14b16ba09882c04c3b5b6082516a601480deeebd
Greger Stolt Nilsen <gregersn@gmail.com>
2022-04-13 12:30:10 +0200

    Add noise octaves.

--- include/pyro/noise.h
+++ include/pyro/noise.h
@@ -4,6 +4,7 @@
 namespace Pyro
 {
     void noiseseed(long seed);
+    void noisedetail(unsigned int lod, float falloff);
     double noise(double x, double y);
     double noise(double x, double y, double z);
     double noise(double x, double y, double z, double w);
