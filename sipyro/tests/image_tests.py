1981bbd2e6b052fd294fd04fcdec8598489016b6
Greger Stolt Nilsen <gregersn@gmail.com>
2022-06-11 12:35:08 +0200

    Fix more python bindings. Add some tests.

--- sipyro/tests/image_tests.py
+++ sipyro/tests/image_tests.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
 
 from PIL import Image
 import numpy as np
