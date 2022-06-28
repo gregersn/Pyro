5b89367391d573999ab059c99ee1e1327cb88bda
Greger Stolt Nilsen <gregersn@gmail.com>
2022-06-28 10:12:11 +0200

    Add point_in_shape function, and test.

---
+++ tests/test-shape.cpp
@@ -0,0 +1,33 @@
+#include <catch2/catch.hpp>
+
+#include "pyro/shape.h"
+
+TEST_CASE("Check if a point is inside a shape",
+          "[shape]")
+{
+    SECTION("Point inside square")
+    {
+        Pyro::Shape shape = Pyro::Shape();
+
+        shape.begin();
+        shape.vertex(0, 0);
+        shape.vertex(10, 0);
+        shape.vertex(10, 10);
+        shape.vertex(0, 10);
+
+        REQUIRE(shape.is_point_in_path(5, 5));
+    }
+
+    SECTION("Point outside square")
+    {
+        Pyro::Shape shape = Pyro::Shape();
+
+        shape.begin();
+        shape.vertex(0, 0);
+        shape.vertex(10, 0);
+        shape.vertex(10, 10);
+        shape.vertex(0, 10);
+
+        REQUIRE(!shape.is_point_in_path(15, 51));
+    }
+}
