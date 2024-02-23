48597b9334b0dd4ca489c3126ba750664a0eb8eb
Greger Stolt Nilsen <gregersn@gmail.com>
2024-02-24 00:11:30 +0100

    Fix exports on Mac

--- configure_mac.sh
+++ configure_mac.sh
@@ -3,6 +3,6 @@
 export LDFLAGS="-L/usr/local/opt/jpeg-turbo/lib"
 export CPPFLAGS="-I/usr/local/opt/jpeg-turbo/include"
 export PKG_CONFIG_PATH="/usr/local/opt/jpeg-turbo/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig"
-export CC=x86_64-apple-darwin22-gcc-13
-export CXX=x86_64-apple-darwin22-g++-13
+export CC=gcc-13
+export CXX=g++-13
 meson setup build
