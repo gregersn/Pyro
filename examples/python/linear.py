e9f4960cb2ea83988cc35c65d87a7ba3b506192c
Greger Stolt Nilsen <gregersn@gmail.com>
2024-04-03 16:58:07 +0200

    Work

--- examples/python/linear.py
+++ examples/python/linear.py
@@ -1,20 +1,18 @@
-import sys
 import pyro
-import time
 
 a = 0
 
 def setup():
     global a
-    pyro.size(640, 360);
-    pyro.stroke(255);
-    a = pyro.height / 2;
+    pyro.size(640, 360)
+    pyro.stroke(255)
+    a = pyro.height / 2
 
 def draw():
     global a
-    pyro.background(51);
-    pyro.line(0, a, pyro.width, a);
-    a = a - 0.5;
+    pyro.background(51)
+    pyro.line(0, a, pyro.width, a)
+    a = a - 0.5
     if a < 0:
         a = pyro.height
 
--- templates/static_graphics.py
+++ templates/static_graphics.py
@@ -1,3 +1,6 @@
+"""Static pyro skech template."""
+from pathlib import Path
+
 import pyro
 
 
@@ -6,11 +9,10 @@ HEIGHT = 15
 DPI = 72
 UNIT = pyro.Unit.mm
 
-pg = pyro.create(WIDTH, HEIGHT, UNIT, DPI)
-
-
 def main():
-    print("Hello")
+    """Main graphics creatiion."""
+    pg = pyro.create(WIDTH, HEIGHT, UNIT, DPI)
+    pg.save(f"{Path(__file__).stem}_{WIDTH}{UNIT.name}_x_{HEIGHT}{UNIT.name}_@_{DPI}_dpi.png", DPI)
 
 
 if __name__ == '__main__':
