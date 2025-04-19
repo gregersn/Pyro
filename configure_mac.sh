a91110e37aa4f3a94c4de84c0deadf3d45dd783a
Greger Stolt Nilsen <gregersn@gmail.com>
2025-04-19 10:52:26 +0200

    Update mac configuration script.

--- configure_mac.sh
+++ configure_mac.sh
@@ -3,6 +3,6 @@
 export LDFLAGS="-L/usr/local/opt/jpeg-turbo/lib"
 export CPPFLAGS="-I/usr/local/opt/jpeg-turbo/include"
 export PKG_CONFIG_PATH="/usr/local/opt/jpeg-turbo/lib/pkgconfig:/usr/local/opt/libffi/lib/pkgconfig"
-export CC=gcc-13
-export CXX=g++-13
+export CC=gcc-14
+export CXX=g++-14
 meson setup build
